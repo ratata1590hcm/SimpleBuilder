7bbeaa14dc0ae6cb6a6318239a4fa5d9459c5f05
ledanghuy <ledanghuy1590@gmail.com>
2024-07-31 21:21:58 +0700

    update

--- .github/actions/task/trivy_scan/run.sh
+++ .github/actions/task/trivy_scan/run.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-trivy image --severity CRITICAL --exit-code 1 test/test:latest
+
+# Get a list of all Docker images
+images=$(docker images --format "{{.Repository}}:{{.Tag}}")
+
+# Loop through each image and scan with Trivy for critical vulnerabilities
+for image in ${images}; do
+	echo "Scanning ${image} for critical vulnerabilities..."
+	output=$(trivy image --severity CRITICAL --exit-code 1 --format json "${image}")
+	echo "${output}" | jq '.' >"${image//:/_}_critical_vulnerabilities.json"
+
+	critical_vulnerabilities=$(echo "${output}" | jq -r '.Results[].Vulnerabilities[] | select(.Severity == "CRITICAL") | .VulnerabilityID')
+
+	if [[ -z ${critical_vulnerabilities} ]]; then
+		echo "No critical vulnerabilities found for ${image}."
+	else
+		echo "Critical vulnerabilities found for ${image}:"
+		echo "${critical_vulnerabilities}"
+	fi
+done
