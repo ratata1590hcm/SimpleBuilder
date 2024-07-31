8740c1bd6e47b3657839042abdc1bb371889453f
ledanghuy <ledanghuy1590@gmail.com>
2024-07-31 18:10:30 +0700

    update

--- .github/actions/task/trunk_check/run.sh
+++ .github/actions/task/trunk_check/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-if [ ! -d ".trunk" ]; then
-  echo "Trunk is not initialized. Initializing..."
-  trunk init
-  echo "Trunk initialization complete."
+if [[ ! -d ".trunk" ]]; then
+	echo "Trunk is not initialized. Initializing..."
+	trunk init
+	echo "Trunk initialization complete."
 else
-  echo "Trunk is already initialized."
+	echo "Trunk is already initialized."
 fi
 
 # Run Trunk check
 echo "Running Trunk check..."
-trunk check
+trunk check -a
