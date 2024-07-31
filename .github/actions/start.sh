e709a90a79c06e2b8e962c880a1218eb67246361
ledanghuy <ledanghuy1590@gmail.com>
2024-07-31 22:07:42 +0700

    update

--- .github/actions/start.sh
+++ .github/actions/start.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-docker compose run --rm 'done'
-docker compose logs --tail all
+
+# Function to handle script exit
+cleanup() {
+	echo "export logs..."
+	docker-compose logs --tail all >build.log
+	cat build.log
+	echo "Pushing images to registry..."
+	docker-compose push
+	echo "Taking down the environment..."
+	docker-compose down -v
+}
+
+# Set the trap to call cleanup function on EXIT signal
+trap cleanup EXIT
+
+# Start the containers in the background
+docker-compose up -d
