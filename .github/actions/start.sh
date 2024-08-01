8bb7477252bf8aed2edb7385da43aa64c2cee5cd
ledanghuy <ledanghuy1590@gmail.com>
2024-08-01 09:03:44 +0700

    update

--- .github/actions/start.sh
+++ .github/actions/start.sh
@@ -5,14 +5,15 @@ cleanup() {
 	echo "export logs..."
 	docker-compose logs --tail all >build.log
 	cat build.log
-    echo "Pushing images to registry..."
-    docker-compose push > /dev/null 2>&1
-    echo "Taking down the environment..."
-    docker-compose down -v > /dev/null 2>&1
+	echo "Pushing images to registry..."
+	docker-compose push >/dev/null 2>&1
+	echo "Taking down the environment..."
+	docker-compose down -v >/dev/null 2>&1
 }
 
 # Set the trap to call cleanup function on EXIT signal
 trap cleanup EXIT
 
 # Start the containers in the background
+docker-compose build >/dev/null 2>&1
 docker-compose up -d
