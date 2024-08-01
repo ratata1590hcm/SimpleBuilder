913cb2b8827cd3a3e92775d2e7b8e0e798326373
ledanghuy <ledanghuy1590@gmail.com>
2024-08-01 09:48:28 +0700

    update

---
+++ .github/actions/task/sonar_scan/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+env_file=".env.sonar"
+
+# Load environment variables from a .env file if it exists
+if [[ -f .env.sonar ]]; then
+	source .env.sonar
+else
+    echo "skip sonar scan .env.sonar not exist"
+fi
+
+scan() {
+    echo "start scan";
+    SONAR_HOST_URL=$SONAR_HOST_URL
+    SONAR_SCANNER_OPTS=-Dsonar.projectKey=$SONAR_PROJECT_KEY
+    SONAR_TOKEN=$SONAR_TOKEN
+    sonar-scanner
+}
+
+if [ ! -f "$env_file" ]; then
+    if [ -z "$SONAR_HOST_URL" ] || [ -z "$SONAR_PROJECT_KEY" ] || [ -z "$SONAR_TOKEN" ]; then 
+        echo "ERROR: SONAR_HOST_URL and SONAR_PROJECT_KEY and SONAR_TOKEN must be set.";
+    else
+        scan
+    fi
+else
+    scan
+fi
