5e341fd3c8be7ca40cb3f06a1403f67eb1afff83
ledanghuy <ledanghuy>
2024-08-02 15:01:38 +0700

    update

--- .github/actions/task/maven_build/run.sh
+++ .github/actions/task/maven_build/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn -s /workspace/.m2/settings.xml -B -q clean install
+mvn -s /workspace/.m2/settings.xml clean install -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
