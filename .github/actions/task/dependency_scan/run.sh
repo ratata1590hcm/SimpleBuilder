77bc8d567721fb3ed101e8742669013dd50fc5d1
external.anh.nguyenhoang5@bcn.bosch.com <external.anh.nguyenhoang5@bcn.bosch.com>
2024-09-12 15:31:24 +0700

    Add

--- .github/actions/task/dependency_scan/run.sh
+++ .github/actions/task/dependency_scan/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
-/usr/share/dependency-check/bin/dependency-check.sh --scan source_code --format "ALL" --project "dependency-check scan" --out ./report
+export JAVA_OPTS="${JAVA_OPTS} -Dhttp.proxyHost=172.18.0.1 -Dhttp.proxyPort=3128 -Dhttps.proxyHost=172.18.0.1 -Dhttps.proxyPort=3128"
+/usr/share/dependency-check/bin/dependency-check.sh --nvdApiKey "${NVD_APIKEY}" --scan source_code --format "ALL" --project "dependency-check scan" --out ./report
 node /action/check_report.js ./report/dependency-check-report.json
 echo "dependency check passed!"
