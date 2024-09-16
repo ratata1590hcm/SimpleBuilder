d93d5599778a9dd6636baf4dfa42d5465ae3a28d
hoanganht1k27 <hoanganh8d@gmail.com>
2024-09-16 10:33:12 +0700

    Update sonar scan

--- .github/actions/task/sonar_scan/run.sh
+++ .github/actions/task/sonar_scan/run.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
-# env_file=".env.sonar"
+env_file=".env.sonar"
 
-# # Load environment variables from a .env file if it exists
-# if [[ -f ".env.sonar" ]]; then
-# 	source .env.sonar
-# else
-# 	echo "skip sonar scan .env.sonar not exist"
-# 	exit
-# fi
+# Load environment variables from a .env file if it exists
+if [[ -f ".env.sonar" ]]; then
+	source .env.sonar
+else
+	echo "skip sonar scan .env.sonar not exist"
+	exit
+fi
 
-# scan() {
-# 	echo "start scan"
-# 	SONAR_HOST_URL="${SONAR_HOST_URL}"
-# 	SONAR_SCANNER_OPTS="-Dsonar.projectKey=${SONAR_PROJECT_KEY}"
-# 	SONAR_TOKEN="${SONAR_TOKEN}"
-# 	sonar-scanner
-# }
+scan() {
+	echo "start scan"
+	echo ${SONAR_HOST_URL}
+	sonar-scanner \
+		-Dsonar.projectKey="${SONAR_PROJECT_KEY}" \
+		-Dsonar.sources="/workspace/${SOURCE_CODE_DIR}" \
+		-Dsonar.host.url="${SONAR_HOST_URL}" \
+		-Dsonar.login="${SONAR_TOKEN}"
+	# sonar-scanner \
+	# 	-Dsonar.projectKey=test \
+	# 	-Dsonar.sources=/workspace/source_code \
+	# 	-Dsonar.host.url=http://192.168.0.1:9001 \
+	# 	-Dsonar.login=sqp_a2069a5b96c474a727eb845d695b6991f7d15864
+}
 
-# if [[ ! -f ${env_file} ]]; then
-# 	if [[ -z ${SONAR_HOST_URL} ]] || [[ -z ${SONAR_PROJECT_KEY} ]] || [[ -z ${SONAR_TOKEN} ]]; then
-# 		echo "ERROR: SONAR_HOST_URL and SONAR_PROJECT_KEY and SONAR_TOKEN must be set."
-# 	else
-# 		scan
-# 	fi
-# else
-# 	scan
-# fi
-
-sonar-scanner \
-	-Dsonar.projectKey=test \
-	-Dsonar.sources=/workspace/source_code/. \
-	-Dsonar.host.url=http://172.18.0.1:9001 \
-	-Dsonar.login=sqp_dec118e0491b0f5c69c94f6031b30b0eb65a1413
+if [[ ! -f ${env_file} ]]; then
+	if [[ -z ${SONAR_HOST_URL} ]] || [[ -z ${SONAR_PROJECT_KEY} ]] || [[ -z ${SONAR_TOKEN} ]] || [[ -z ${SOURCE_CODE_DIR} ]]; then
+		echo "ERROR: SONAR_HOST_URL and SONAR_PROJECT_KEY and SONAR_TOKEN and SOURCE_CODE_DIR must be set."
+	else
+		scan
+	fi
+else
+	scan
+fi
